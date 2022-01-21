696a1738a5829a8172400e454e1c7531ac68d614
Noah Marconi <noah.marconi@gmail.com>
2022-01-21 11:37:00 -0500

    debug task placeholder

---
+++ scripts/debug.ts
@@ -0,0 +1,17 @@
+import { task } from 'hardhat/config'
+import '@nomiclabs/hardhat-ethers'
+import { Logger } from 'tslog'
+import config from './config/config'
+import { ethers } from 'ethers'
+import { MockERC20 } from '../dist/types'
+
+const logger: Logger = new Logger()
+
+task('debug-erc20', 'Debug calls')
+    .addParam("address", "token address to call")
+    .setAction(async (args, hre) => {
+        const instance = await hre.ethers.getContractAt("MockERC20", args.address) as MockERC20;
+
+        logger.info(instance.balanceOf(ethers.constants.AddressZero));
+    })
+
